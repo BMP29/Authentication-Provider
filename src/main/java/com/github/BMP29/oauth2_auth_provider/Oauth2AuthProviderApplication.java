a643de0b44291e87121849653ae8688c3b30bf01
Breno <brenomp730@gmail.com>
2026-06-16 23:57:43 -0300

    Inicializando projeto

---
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/Oauth2AuthProviderApplication.java
@@ -0,0 +1,13 @@
+package com.github.BMP29.oauth2_auth_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AuthProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/BMP29/oauth2_auth_provider/Oauth2AuthProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.BMP29.oauth2_auth_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AuthProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
