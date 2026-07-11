3e9c2bb5510e380337d032b9c532b61a058bff62
Breno <brenomp730@gmail.com>
2026-07-11 15:25:20 -0300

    feat: define beans e regras de acesso
    Libera endpoints públicos sob o caminho "/auth/*"
    Adiciona Bean do UserDetailsService
    Adiciona Bean do BCryptPasswordEncoder
    Adiciona Bean do AuthenticationManager

--- src/main/java/com/github/BMP29/oauth2_auth_provider/config/SecurityConfig.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/config/SecurityConfig.java
@@ -5,8 +5,14 @@ import com.github.BMP29.oauth2_auth_provider.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,12 +22,29 @@ public class SecurityConfig {
     private UserRepository userRepository;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
                 authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .requestMatchers("/auth/*").permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
     }
 }
