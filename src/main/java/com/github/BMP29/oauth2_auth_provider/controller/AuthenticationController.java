772cba07949a3747819c31a1a556d41433a3734d
Breno <brenomp730@gmail.com>
2026-07-11 15:20:07 -0300

    feat: adiciona controller de autenticacao e signup endpoint

--- src/main/java/com/github/BMP29/oauth2_auth_provider/controller/AuthenticationController.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/controller/AuthenticationController.java
@@ -1,9 +1,13 @@
 package com.github.BMP29.oauth2_auth_provider.controller;
 
-import com.github.BMP29.oauth2_auth_provider.dto.UserDto;
+import com.github.BMP29.oauth2_auth_provider.dto.*;
+import com.github.BMP29.oauth2_auth_provider.entity.User;
+import com.github.BMP29.oauth2_auth_provider.mapper.UserMapper;
+import com.github.BMP29.oauth2_auth_provider.service.Impl.AuthenticationServiceImpl;
 import com.github.BMP29.oauth2_auth_provider.service.Impl.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthenticationController {
-    private UserServiceImpl userService;
+    private AuthenticationServiceImpl authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> register(@RequestBody UserDto userDto) {
-        userService.createUser(userDto);
+    public ResponseEntity<StandardResponseDto<UserDto>> register(@RequestBody SignUpDto signUpInput) {
+        User u = authService.signup(signUpInput);
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED).build();
+        UserDto userDto = UserMapper.mapToUserDto(u);
+
+        StandardResponseDto<UserDto> stdResponse =
+                new StandardResponseDto<>(
+                        true,
+                        "Usuário criado com sucesso",
+                        userDto);
+
+        return new ResponseEntity<>(stdResponse, HttpStatus.CREATED);
     }
 }
