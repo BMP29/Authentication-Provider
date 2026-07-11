ce070a467996d425abc9c98362b42f38021b0531
Breno <brenomp730@gmail.com>
2026-07-11 15:09:48 -0300

    refactor: remove assinatura de criacao de usuario

--- src/main/java/com/github/BMP29/oauth2_auth_provider/service/IUserService.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/service/IUserService.java
@@ -1,7 +1,4 @@
 package com.github.BMP29.oauth2_auth_provider.service;
 
-import com.github.BMP29.oauth2_auth_provider.dto.UserDto;
-
 public interface IUserService {
-    public void createUser(UserDto userDto);
 }
