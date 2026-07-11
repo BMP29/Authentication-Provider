580de4ef9c02e87f16e28617601850d54156fc7e
Breno <brenomp730@gmail.com>
2026-07-11 15:42:19 -0300

    refactor: adapta o mapper ao novo UserDto e remove mapToUser

--- src/main/java/com/github/BMP29/oauth2_auth_provider/mapper/UserMapper.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/mapper/UserMapper.java
@@ -4,19 +4,15 @@ import com.github.BMP29.oauth2_auth_provider.dto.UserDto;
 import com.github.BMP29.oauth2_auth_provider.entity.User;
 
 public class UserMapper {
-    public static UserDto mapToUserDto(User user, UserDto userDto) {
-        userDto.setUsername(user.getUsername());
-        userDto.setEmail(user.getEmail());
-        userDto.setPassword(user.getPassword());
+    public static UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto(
+                user.getUsername(),
+                user.getEmail(),
+                user.isEnabled(),
+                user.getCreatedAt()
+        );
 
         return userDto;
     }
-
-    public static User mapToUser(UserDto userDto, User user) {
-        user.setUsername(userDto.getUsername());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-
-        return user;
-    }
+    
 }
