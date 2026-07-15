b8963da1988005ab35f8b3c0944701cdb18ff970
Breno <brenomp730@gmail.com>
2026-07-14 23:38:46 -0300

    feat: adiciona envio de codigo de verificacao por email

--- src/main/java/com/github/BMP29/oauth2_auth_provider/service/Impl/AuthenticationServiceImpl.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/service/Impl/AuthenticationServiceImpl.java
@@ -5,6 +5,7 @@ import com.github.BMP29.oauth2_auth_provider.dto.VerifyUserDto;
 import com.github.BMP29.oauth2_auth_provider.entity.User;
 import com.github.BMP29.oauth2_auth_provider.repository.UserRepository;
 import com.github.BMP29.oauth2_auth_provider.service.IAuthenticationService;
+import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,6 +22,7 @@ public class AuthenticationServiceImpl implements IAuthenticationService {
     private UserRepository userRepository;
     private AuthenticationManager authenticationManager;
     private BCryptPasswordEncoder passwordEncoder;
+    private EmailServiceImpl emailService;
 
     @Override
     public User signup(SignUpDto signUpInput) {
@@ -32,6 +34,7 @@ public class AuthenticationServiceImpl implements IAuthenticationService {
         user.setCreatedAt(LocalDateTime.now());
         user.setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(15));
         user.setVerificationCode(UUID.randomUUID().toString());
+        sendVerificationEmail(user);
 
         return userRepository.save(user);
     }
@@ -55,4 +58,42 @@ public class AuthenticationServiceImpl implements IAuthenticationService {
         user.setVerificationCodeExpiresAt(null);
         userRepository.save(user);
     }
+
+
+    private void sendVerificationEmail(User user) {
+        String subject = "Account Verification";
+        String verificationCode = "VERIFICATION CODE " + user.getVerificationCode();
+        String htmlMessage = "<html>"
+                + "<body style=\"font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color: #f3f3f3; margin: 0; padding: 0;\">"
+                + "<div style=\"width: 100%; max-width: 600px; margin: 0 auto; padding: 30px;\">"
+                + "<div style=\"text-align: center; background-color: #4CAF50; color: #fff; padding: 20px; border-radius: 8px 8px 0 0;\">"
+                + "<h2 style=\"margin: 0; font-size: 24px;\">Bem-vindo ao nosso App!</h2>"
+                + "<p style=\"font-size: 16px;\">Estamos felizes em tê-lo conosco. Complete o processo abaixo para continuar.</p>"
+                + "</div>"
+                + "<div style=\"background-color: #ffffff; padding: 30px; border-radius: 0 0 8px 8px; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);\">"
+                + "<h3 style=\"color: #333; font-size: 22px; margin-bottom: 15px;\">Código de Verificação</h3>"
+                + "<p style=\"font-size: 16px; color: #555;\">Digite o código de verificação abaixo para continuar:</p>"
+                + "<div style=\"text-align: center; margin-top: 20px;\">"
+                + "<span style=\"font-size: 28px; font-weight: bold; color: #4CAF50; padding: 10px 20px; background-color: #f1f1f1; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\">"
+                + verificationCode
+                + "</span>"
+                + "</div>"
+                + "<p style=\"font-size: 14px; color: #888; margin-top: 30px;\">Se você não reconhece essa solicitação, ignore este email.</p>"
+                + "</div>"
+                + "<div style=\"text-align: center; margin-top: 40px; font-size: 12px; color: #999;\">"
+                + "<p>&copy; 2026, Nossa Empresa. Todos os direitos reservados.</p>"
+                + "</div>"
+                + "</div>"
+                + "</body>"
+                + "</html>";
+
+        try {
+            emailService.sendVerificationEmail(user.getEmail(), subject, htmlMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //fazer: endpoint para verificar a conta
+    //fazer: endpoint para registrar clients
 }
