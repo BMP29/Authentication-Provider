de7a8d5b1c3cfaa10d6726a455323c4ce40f36c7
Breno <brenomp730@gmail.com>
2026-07-14 23:03:19 -0300

    feat: adiciona assinatura do metodo de verificação

--- src/main/java/com/github/BMP29/oauth2_auth_provider/service/IAuthenticationService.java
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/service/IAuthenticationService.java
@@ -1,8 +1,10 @@
 package com.github.BMP29.oauth2_auth_provider.service;
 
 import com.github.BMP29.oauth2_auth_provider.dto.SignUpDto;
+import com.github.BMP29.oauth2_auth_provider.dto.VerifyUserDto;
 import com.github.BMP29.oauth2_auth_provider.entity.User;
 
 public interface IAuthenticationService {
     public User signup(SignUpDto signUpInput);
+    public void verify(VerifyUserDto verifyUserDto);
 }
