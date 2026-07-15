bf56f6320e9778af1bebb153059db9f165d4cf0b
Breno <brenomp730@gmail.com>
2026-07-14 23:41:28 -0300

    feat: adiciona servico de envio de email

---
+++ src/main/java/com/github/BMP29/oauth2_auth_provider/service/Impl/EmailServiceImpl.java
@@ -0,0 +1,26 @@
+package com.github.BMP29.oauth2_auth_provider.service.Impl;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailServiceImpl {
+    private JavaMailSender javaMailSender;
+
+    public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
+        javaMailSender.send(message);
+    }
+
+}
